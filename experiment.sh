e8429fdab69ed5d16b1b1cd2f3868a3a6fcf83e3
Gaurav Manek <gaurav@gauravmanek.com>
2017-04-03 12:39:32 +0800

    Revert change to experiment.sh

--- experiment.sh
+++ experiment.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
-# Suppress info and warning logs
-export TF_CPP_MIN_LOG_LEVEL=2
+# Suppress info logs
+export TF_CPP_MIN_LOG_LEVEL=1
 python3 adversarial.py \
+    --resume \
     --preprocessor  normalize \
     --generator     simple_model \
     --discriminator simple_model \
