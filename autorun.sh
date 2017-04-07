0cf063da032fa25ed81c468aa233f76c9c6795fc
Gaurav Manek <gaurav@gauravmanek.com>
2017-04-07 15:10:37 +0800

    [Partial] First draft.

---
+++ autorun.sh
@@ -0,0 +1,53 @@
+#!/bin/bash
+
+EXP_DIR="experiments"
+EXP_LOG="$EXP_DIR/log.txt"
+
+if [ ! -d .git ]; then
+    echo "You must run this while in the same folder as the repository."
+    echo "Exiting..."
+    exit 1
+fi
+
+if [ ! -d "$EXP_DIR" ]; then
+    mkdir "$EXP_DIR"
+    touch "$EXP_LOG"
+else
+    echo "$(date) Started experiments" > "$EXP_LOG"
+fi
+
+rm -r "train_logs"
+rm -r "weights"
+
+for commithash in "$@"
+do
+    if [ -f experiment.sh ]; then
+        echo "$(date) Skipping $commithash, experiment.sh not found." >> "$EXP_LOG"
+        continue
+    fi
+
+    expdir = "$EXP_DIR/$commithash"
+
+    if [ -e "$expdir" ]; then
+        echo "$(date) Skipping $commithash, $expdir already exists." >> "$EXP_LOG"
+        continue
+    fi
+
+    echo "$(date) Starting $commithash" >> "$EXP_LOG"
+    mkdir "$expdir"
+    git reset --hard HEAD
+    git pull --all
+    if ! git checkout $commithash ; then
+        echo "$(date) Skipping $commithash, cannot checkout." >> "$EXP_LOG"
+        continue
+    fi
+
+    # Run the experiment
+    "./experiment.sh"
+
+    echo "$(date) Moving output folders." >> "$EXP_LOG"
+    mv "train_logs" "$commithash/"
+    mv "weights" "$commithash/"
+
+    echo "$(date) Done!" >> "$EXP_LOG"
+done
