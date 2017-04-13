3b2b0aab5891e54baf427d93a5edbb4c004931c5
Gaurav Manek <gaurav@gauravmanek.com>
2017-04-13 13:47:44 +0800

    [Partial] Updated metrics.

--- support.py
+++ support.py
@@ -76,7 +76,7 @@ def label_fake(y_true, y_pred):
     assert Y_FAKE == 1
     return K.round(K.clip(y_pred, 0., 1.))
 
-METRICS = [label_real, label_fake]
+METRICS = [label_real, label_fake, 'accuracy']
 
 def get_metric_names(com_model, dis_model_labelled, dis_model_unlabelled, gen_model):
     # Keras overwrites the names of metrics, so here we check that their order is as expected before creating a custom
@@ -84,14 +84,14 @@ def get_metric_names(com_model, dis_model_labelled, dis_model_unlabelled, gen_mo
     logger.debug("Metrics for gen_model: {}".format(gen_model.metrics_names))
     assert gen_model.metrics_names == ['loss']
     logger.debug("Metrics for dis_model_labelled: {}".format(dis_model_labelled.metrics_names))
-    assert dis_model_labelled.metrics_names == ['loss', 'discriminator_loss', 'classifier_loss','discriminator_label_real', 'discriminator_label_fake', 'classifier_label_real', 'classifier_label_fake']
+    assert dis_model_labelled.metrics_names == ['loss', 'discriminator_loss', 'classifier_loss', 'discriminator_label_real', 'discriminator_label_fake', 'discriminator_acc', 'classifier_label_real', 'classifier_label_fake', 'classifier_acc']
     logger.debug("Metrics for dis_model_unlabelled: {}".format(dis_model_unlabelled.metrics_names))
-    assert dis_model_unlabelled.metrics_names == ['loss', 'discriminator_loss', 'classifier_loss', 'discriminator_label_real', 'discriminator_label_fake', 'classifier_label_real', 'classifier_label_fake']
+    assert dis_model_unlabelled.metrics_names == ['loss', 'discriminator_loss', 'classifier_loss', 'discriminator_label_real', 'discriminator_label_fake', 'discriminator_acc', 'classifier_label_real', 'classifier_label_fake', 'classifier_acc']
     logger.debug("Metrics for com_model: {}".format(com_model.metrics_names))
-    assert com_model.metrics_names == ['loss', 'model_discriminator_unlabelled_loss', 'model_discriminator_unlabelled_loss', 'model_discriminator_unlabelled_label_real', 'model_discriminator_unlabelled_label_fake', 'model_discriminator_unlabelled_label_real', 'model_discriminator_unlabelled_label_fake']
+    assert com_model.metrics_names == ['loss', 'model_discriminator_unlabelled_loss', 'model_discriminator_unlabelled_loss', 'model_discriminator_unlabelled_label_real', 'model_discriminator_unlabelled_label_fake', 'model_discriminator_unlabelled_acc', 'model_discriminator_unlabelled_label_real', 'model_discriminator_unlabelled_label_fake', 'model_discriminator_unlabelled_acc']
     
     # Custom name array.
-    return ['loss', 'discriminator_loss', 'classifier_loss', 'discriminator_label_real', 'discriminator_label_fake', 'classifier_label_real', 'classifier_label_fake']
+    return ['loss', 'discriminator_loss', 'classifier_loss', 'discriminator_label_real', 'discriminator_label_fake', 'discriminator_acc', 'classifier_label_real', 'classifier_label_fake', 'classifier_acc']
 
 # TODO: Support reading test data.
 
