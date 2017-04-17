7afddd8282a1cc82f096cca6a4fa843d7211a8be
Ravoxsg <mathieu.ravaut@student.ecp.fr>
2017-04-17 16:49:31 +0800

    A quick batch-normalization implementation

--- models/simple_model.py
+++ models/simple_model.py
@@ -7,7 +7,10 @@ from keras import layers, models
 from keras.layers import (Activation, Conv2D, Conv2DTranspose, Dense, Dropout,
                           Flatten, Reshape)
 from keras.layers.advanced_activations import LeakyReLU
+from keras.layers.core import Lambda
 from keras.models import Sequential
+from keras.layers.normalization import BatchNormalization
+from keras import backend as K
 
 #
 # GENERATOR
@@ -22,6 +25,13 @@ IMAGE_DIM = (32, 32, 3)
 #leaky relu coefficient
 alpha = 0.3
 
+#hand-made normalization
+def normalization(x):
+    x -= K.mean(x, axis=0, keepdims=True)
+    x /= K.std(x, axis=0, keepdims=True)
+    return x
+
+
 def generator(input_size, output_size) -> layers.convolutional._Conv:
     # We only allow the discriminator model to work on CIFAR-sized data.
     assert output_size == (32, 32, 3)
@@ -30,17 +40,21 @@ def generator(input_size, output_size) -> layers.convolutional._Conv:
     y = x
     lys = [
         Dense(256),
+        Lambda(normalization),
         LeakyReLU(alpha),
         Reshape((4, 4, -1)),
         Conv2DTranspose(256, (3, 3), padding = 'same', strides=(2, 2)),
+        Lambda(normalization),
         LeakyReLU(alpha),
-        Dropout(0.5),
+		Dropout(0.5),
         Conv2DTranspose(128, (3, 3), padding = 'same', strides=(2, 2)),
+        Lambda(normalization),
         LeakyReLU(alpha),
-        Dropout(0.5),
+		Dropout(0.5),
         Conv2DTranspose(64, (3, 3), padding = 'same', strides=(2, 2)),
+        Lambda(normalization),
         LeakyReLU(alpha),
-        Dropout(0.5),
+		Dropout(0.5),
         Conv2DTranspose(3, (1, 1), activation='tanh', padding='same')
     ]
 
@@ -68,13 +82,17 @@ def discriminator(input_size):
     lys = [
     	Dense(256),
     	Conv2D(128, (3, 3), padding='same', strides=(2, 2)),
+        Lambda(normalization),
     	LeakyReLU(alpha),
        	Conv2D(256, (3, 3), padding='same', strides=(2, 2)),
+        Lambda(normalization),
     	LeakyReLU(alpha),
     	Conv2D(512, (3, 3), padding='same', strides=(2, 2)),
+        Lambda(normalization),
     	LeakyReLU(alpha),
     	Flatten(),
         Dense(16),
+        Lambda(normalization),
         LeakyReLU(alpha),
         Dense(1, activation='sigmoid', name='discriminator')
     ]
