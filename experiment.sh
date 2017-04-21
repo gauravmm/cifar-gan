b9a0bf913aa6fb0cae7a015e83b8eeada265eac8
Gaurav Manek <gaurav@gauravmanek.com>
2017-04-21 17:12:39 +0800

    Undo experiment.sh changes.

--- experiment.sh
+++ experiment.sh
@@ -2,11 +2,10 @@
 # Suppress info logs
 export TF_CPP_MIN_LOG_LEVEL=2
 python3 adversarial.py           \
-    --resume \
     --preprocessor  normalize    \
     --generator     simple_model \
     --discriminator simple_model \
-    --log-interval 1            \
+    --log-interval 50            \
     --batches 2500               \
     --image-columns 4            \
     train
