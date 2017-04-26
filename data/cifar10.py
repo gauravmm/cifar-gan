f84974f81d65583170faa3541f1476bdbd0812d9
Gaurav Manek <gaurav@gauravmanek.com>
2017-04-26 10:44:22 +0800

    Updated dataset loader to remove Keras dependency.

--- data/cifar10.py
+++ data/cifar10.py
@@ -1,11 +1,16 @@
 # CIFAR10 Downloader
 
 import logging
+import pickle
 import math
+import os
+import errno
+import tarfile
+import shutil
 
 import numpy as np
 
-from keras.datasets import cifar10
+import urllib3
 
 logger = logging.getLogger(__name__)
 
@@ -87,34 +92,91 @@ def get_data(split, batch_size, labelled_fraction=1.0):
                     ndarray: (batch_size, 10, 1)     1-hot label encoding
 
     """
-
-    # (x_train, y_train), (x_test, y_test) = cifar10.load_data()
-    train, test = cifar10.load_data()
-    # Shuffle the training data
-    if _shuffle:
-        x, y = train
-        p = np.random.permutation(x.shape[0])
-        x = x[p,...]
-        y = y[p,...]
-        train = (x, y)
-
-    # Restrict the number of training examples
-    if _data_frac < 1.0:
-        x, y = train
-        n = x.shape[0] * _data_frac
-        x = x[:n,...]
-        y = y[:n,...]
-        train = (x, y)
-
-    num_lbl = math.floor(train[0].shape[0] * labelled_fraction)
-
+    
     if split == "train":
+        train = _get_dataset("train")
+        # Shuffle the training data
+        if _shuffle:
+            x, y = train
+            p = np.random.permutation(x.shape[0])
+            x = x[p,...]
+            y = y[p,...]
+            train = (x, y)
+
+        # Restrict the number of training examples
+        if _data_frac < 1.0:
+            x, y = train
+            n = x.shape[0] * _data_frac
+            x = x[:n,...]
+            y = y[:n,...]
+            train = (x, y)
+
+        num_lbl = math.floor(train[0].shape[0] * labelled_fraction)
         # Return (unlabelled, labelled).
         # We strip the labels out and replace them with None, to better data hygene
         return (map(lambda x: (x[0], None), gen(train, batch_size)),
                 gen((train[0][:num_lbl,...], train[1][:num_lbl,...]), batch_size))
+
     elif split =="test":
+        test = _get_dataset("test")
         nt = (test[0].shape[0] // batch_size) * batch_size
         return (nt, gen(test, batch_size, False))
     
     assert not "get_data must be called with \"train\" or \"test\"."
+
+
+def _unpickle_file(filename):
+    logger.debug("Loading pickle file: {}".format(filename))
+
+    with open(filename, mode='rb') as file:
+        data = pickle.load(file, encoding='bytes')
+
+    # Reorder the data
+    img = data[b'data']
+    img = img.reshape([-1, 3, 32, 32])
+    img = img.transpose([0, 2, 3, 1])
+    # Load labels
+    lbl = np.array(data[b'labels'])
+
+    return img, lbl
+
+
+def _get_dataset(split):
+    assert split == "test" or split == "train"
+    path = "data"
+    dirname = "cifar-10-batches-py"
+    data_url = "https://www.cs.toronto.edu/~kriz/cifar-10-python.tar.gz"
+
+    if not os.path.exists(os.path.join(path, dirname)):
+        # Extract or download data
+        try:
+            os.makedirs(path)
+        except OSError as exception:
+            if exception.errno != errno.EEXIST:
+                raise
+        
+        file_path = os.path.join(path, data_url.split('/')[-1])
+        if not os.path.exists(file_path):
+            # Download
+            logger.warn("Downloading {}".format(data_url))
+            with urllib3.PoolManager().request('GET', data_url, preload_content=False) as r, \
+                 open(file_path, 'wb') as w:
+                    shutil.copyfileobj(r, w)
+
+        logger.warn("Unpacking {}".format(file_path))
+        # Unpack data
+        tarfile.open(name=file_path, mode="r:gz").extractall(path)
+
+    # Import the data
+    filenames = ["test_batch"] if split == "test" else \
+                ["data_batch_{}".format(i) for i in range(1, 6)]
+    
+    imgs = []
+    lbls = []
+    for f in filenames:
+        img, lbl = _unpickle_file(os.path.join(path, dirname, f))
+        imgs.append(img)
+        lbls.append(lbl)
+
+    # Now we flatten the arrays
+    return np.concatenate(imgs), np.concatenate(lbls)
