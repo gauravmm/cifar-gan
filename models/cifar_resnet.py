Merge fix-normalization from Mathieu. Closes #120.

models/cifar_resnet.py
@@ -153,6 +153,8 @@ def generator(inp, is_training, inp_label, output_size, **kwargs):
     x = tf.layers.conv2d_transpose(x, 3, 4, 2, padding='same',
             kernel_initializer=init_kernel,
             name='tconv4')
-    x = tf.tanh(x, name='tconv4/tanh')
+    
+    # Normalize between 0 and 1:
+    x = (tf.tanh(x, name='tconv4/tanh') + 1.0)/2
 
     return x